a86fe0589da1e2ee0f8626744f73f70c3e593859
cyberassemble <124307071+cyberassemble@users.noreply.github.com>
2023-03-20 16:50:59 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "6173491393:AAFkL95vVKPKBIwZz8li31XaP0Yoz-xObb8")
     FSUB = getenv("FSUB", "assemblechat")
     CHID = int(getenv("CHID", "-1001631522563"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    SUDO = list(map(int, getenv("5136746907"))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://seyam8267:5HQ6NC2FIxbbp9oV@cluster0.niwc9bl.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
